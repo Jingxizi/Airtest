51fd60039454b24509285baca77063ba568f9d5f
GitLab <cczq@qq.com>
2025-08-20 17:00:08 +0800

    Auto commit on 2025-08-20 17:00:08

---
+++ main.sh
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+airtest run "/Users/cczq/software/iOS_Proj/Airtest/test1.air" --device ios:///http://172.23.1.200:8100 --device ios:///http://172.23.1.200:8100 --log "/Users/cczq/software/iOS_Proj/Common/log/airtest"
+
+# airtest run "/Users/cczq/software/iOS_Proj/Airtest/test2.air" --device ios:///http://172.23.1.200:8100 --device ios:///http://172.23.1.200:8100 --log "/Users/cczq/software/iOS_Proj/Common/log/airtest"
+
+
+
